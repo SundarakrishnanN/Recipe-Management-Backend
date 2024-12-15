f76963efb7eeefa5be55bcad9f360f4e9043ce8e
SundarakrishnanN <sundar050804@gmail.com>
2024-12-15 20:26:59 +0530

    update

--- recipemanagement/recipemanagement/urls.py
+++ recipemanagement/recipemanagement/urls.py
@@ -19,10 +19,11 @@ from django.urls import path,include
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import index 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', index, name='index'),
     path('api/', include('login.urls')),
     path('api/', include('recipes.urls')),
 ]
