d3499aa3fddd8aa227e875818ea95a0fffc7bb22
SundarakrishnanN <sundar050804@gmail.com>
2024-12-16 11:08:59 +0530

    update db

--- recipemanagement/recipemanagement/settings.py
+++ recipemanagement/recipemanagement/settings.py
@@ -108,9 +108,13 @@ WSGI_APPLICATION = "recipemanagement.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+  'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_yEwcWsFDjjCNAbqWL4o',
+        'HOST': 'recipe-master-recipe-master1.f.aivencloud.com',  # e.g., 'localhost' or 'your-database-host.com'
+        'PORT': '10605',       # Default MySQL port
     }
 }
 
